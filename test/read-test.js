421fd13161f942dd5e92b36bf13ca509ab855eff
Roly Fentanes <roly426@gmail.com>
2012-07-02 02:39:54 -0700

    new Torrent API

--- test/read-test.js
+++ test/read-test.js
@@ -5,12 +5,15 @@ var nt     = require('..')
   , fs     = require('fs')
   , url    = require('url')
   , nock   = require('nock')
+  ;
+
 
 var file1 = path.join(__dirname, 'torrents', 'ubuntu.torrent')
   , file2 = path.join(__dirname,'torrents', 'click.jpg.torrent')
   , file3 = path.join(__dirname, 'torrents', 'virtualbox.torrent')
   , remotefile3 = 'http://www.mininova.org/get/2886852'
   , file4 = path.join(__dirname, 'torrents', 'chipcheezum.torrent')
+  ;
 
 
 // mock request to remote file
@@ -28,29 +31,30 @@ vows.describe('Read')
       },
 
       'Info hash matches': function(result) {
-        assert.isObject(result);
-        assert.equal(nt.getInfoHash(result),
+        assert.isObject(result.metadata);
+        assert.equal(result.infoHash(),
           'a38d02c287893842a32825aa866e00828a318f07');
       },
       'Announce URL is correct': function(result) {
-        assert.isObject(result);
-        assert.include(result, 'announce');
-        assert.equal(result.announce, 'udp://tracker.publicbt.com:80');
+        assert.isObject(result.metadata);
+        assert.include(result.metadata, 'announce');
+        assert.equal(result.metadata.announce, 'udp://tracker.publicbt.com:80');
       },
       'Single file mode': function(result) {
-        assert.isObject(result);
-        assert.include(result, 'info');
-        assert.include(result.info, 'name');
-        assert.equal(result.info.name, 'ubuntu-11.04-desktop-i386.iso');
-        assert.isUndefined(result.info.files);
-        assert.include(result.info, 'length');
-        assert.equal(result.info.length, 718583808);
+        assert.isObject(result.metadata);
+        assert.include(result.metadata, 'info');
+        assert.include(result.metadata.info, 'name');
+        assert.equal(result.metadata.info.name,
+                     'ubuntu-11.04-desktop-i386.iso');
+        assert.isUndefined(result.metadata.info.files);
+        assert.include(result.metadata.info, 'length');
+        assert.equal(result.metadata.info.length, 718583808);
       },
       '512 KB piece length': function(result) {
-        assert.isObject(result);
-        assert.include(result, 'info');
-        assert.include(result.info, 'piece length');
-        assert.equal(result.info['piece length'], 524288);
+        assert.isObject(result.metadata);
+        assert.include(result.metadata, 'info');
+        assert.include(result.metadata.info, 'piece length');
+        assert.equal(result.metadata.info['piece length'], 524288);
       }
     },
 
@@ -63,35 +67,35 @@ vows.describe('Read')
         },
 
         'Info hash matches': function(result) {
-          assert.isObject(result);
-          assert.equal(nt.getInfoHash(result),
+          assert.isObject(result.metadata);
+          assert.equal(result.infoHash(),
               '2fff646b166f37f4fd131778123b25a01639e0b3');
         },
         'Announce URL is correct': function(result) {
-          assert.isObject(result);
-          assert.include(result, 'announce');
-          assert.equal(result.announce, 'http://hello.2u');
+          assert.isObject(result.metadata);
+          assert.include(result.metadata, 'announce');
+          assert.equal(result.metadata.announce, 'http://hello.2u');
         },
         'Single file mode': function(result) {
-          assert.isObject(result);
-          assert.include(result, 'info');
-          assert.include(result.info, 'name');
-          assert.equal(result.info.name, 'click.jpg');
-          assert.isUndefined(result.info.files);
-          assert.include(result.info, 'length');
-          assert.equal(result.info.length, 87582);
+          assert.isObject(result.metadata);
+          assert.include(result.metadata, 'info');
+          assert.include(result.metadata.info, 'name');
+          assert.equal(result.metadata.info.name, 'click.jpg');
+          assert.isUndefined(result.metadata.info.files);
+          assert.include(result.metadata.info, 'length');
+          assert.equal(result.metadata.info.length, 87582);
         },
         '256 KB piece length': function(result) {
-          assert.isObject(result);
-          assert.include(result, 'info');
-          assert.include(result.info, 'piece length');
-          assert.equal(result.info['piece length'], 262144);
+          assert.isObject(result.metadata);
+          assert.include(result.metadata, 'info');
+          assert.include(result.metadata.info, 'piece length');
+          assert.equal(result.metadata.info['piece length'], 262144);
         },
         'Private torrent': function(result) {
-          assert.isObject(result);
-          assert.include(result, 'info');
-          assert.include(result.info, 'private');
-          assert.equal(result.info.private, 1);
+          assert.isObject(result.metadata);
+          assert.include(result.metadata, 'info');
+          assert.include(result.metadata.info, 'private');
+          assert.equal(result.metadata.info.private, 1);
         }
       },
 
@@ -101,8 +105,8 @@ vows.describe('Read')
         },
 
         'Info hash matches': function(result) {
-          assert.isObject(result);
-          assert.equal(nt.getInfoHash(result),
+          assert.isObject(result.metadata);
+          assert.equal(result.infoHash(),
               'a51cbb0e3b4d6430ca0d1da70c1c7b0bb94304f4');
         }
       }
@@ -115,8 +119,8 @@ vows.describe('Read')
         },
 
         'Info hash matches': function(result) {
-          assert.isObject(result);
-          assert.equal(nt.getInfoHash(result),
+          assert.isObject(result.metadata);
+          assert.equal(result.infoHash(),
               'a51cbb0e3b4d6430ca0d1da70c1c7b0bb94304f4');
         }
       }
@@ -129,22 +133,24 @@ vows.describe('Read')
       },
 
       'Info hash matches': function(result) {
-        assert.isObject(result);
-        assert.equal(nt.getInfoHash(result),
+        assert.isObject(result.metadata);
+        assert.equal(result.infoHash(),
             '6a7eb42ab3b9781eba2d9ff3545d9758f27ec239');
       },
       'Announce URL is correct': function(result) {
-        assert.isObject(result);
-        assert.include(result, 'announce');
-        assert.equal(result.announce, 'http://tracker.mininova.org/announce');
+        assert.isObject(result.metadata);
+        assert.include(result.metadata, 'announce');
+        assert.equal(result.metadata.announce, 'http://tracker.mininova.org/announce');
       },
       'Multi file mode': function(result) {
-        assert.isObject(result);
-        assert.include(result, 'info');
-        assert.include(result.info, 'name');
-        assert.equal(result.info.name, 'VirtualBox - CentOS 4.8 i386 Desktop Virtual Disk Image - [VirtualBoxImages.com]');
-        assert.include(result.info, 'files');
-        assert.deepEqual(result.info.files, [
+        assert.isObject(result.metadata);
+        assert.include(result.metadata, 'info');
+        assert.include(result.metadata.info, 'name');
+        assert.equal(result.metadata.info.name,
+                     'VirtualBox - CentOS 4.8 i386 Desktop Virtual Disk Image' +
+                     ' - [VirtualBoxImages.com]');
+        assert.include(result.metadata.info, 'files');
+        assert.deepEqual(result.metadata.info.files, [
           {
             length: 291,
             path: [ 'Distributed by Mininova.txt' ]
@@ -156,10 +162,10 @@ vows.describe('Read')
         ]);
       },
       '1 MB piece length': function(result) {
-        assert.isObject(result);
-        assert.include(result, 'info');
-        assert.include(result.info, 'piece length');
-        assert.equal(result.info['piece length'], 1048576);
+        assert.isObject(result.metadata);
+        assert.include(result.metadata, 'info');
+        assert.include(result.metadata.info, 'piece length');
+        assert.equal(result.metadata.info['piece length'], 1048576);
       }
     }
   })
